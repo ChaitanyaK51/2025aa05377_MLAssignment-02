2635cc23ffbeef3f999dbb0fe6f5b2c4b07a4c29
ChaitanyaK51 <chaitanyakale343@gmail.com>
2026-01-14 10:27:12 +0530

    Update ML_assignment_02.py

--- ML_assignment_02.py
+++ ML_assignment_02.py
@@ -15,7 +15,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install scikit-learn 
+import scikit-learn as sk
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
