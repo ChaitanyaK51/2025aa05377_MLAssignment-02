49a3a54cad529a5d9fd8305355c8965ea2135b78
ChaitanyaK51 <chaitanyakale343@gmail.com>
2026-02-02 14:18:09 +0530

    Update decision_tree_model.py

--- model/decision_tree_model.py
+++ model/decision_tree_model.py
@@ -1,1 +1,9 @@
+from sklearn.tree import DecisionTreeClassifier
 
+model = DecisionTreeClassifier(
+    max_depth=6,
+    min_samples_split=10,
+    min_samples_leaf=5,
+    criterion="gini",
+    random_state=42
+)
